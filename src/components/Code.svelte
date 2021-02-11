c5d4290b2291166063a96362546a305548dae25c
Dominik Tarnowski <dom@tdom.dev>
2021-02-11 17:42:13 +0000

    Fix Code svelte bug (fails to destroy slot)

--- src/components/Code.svelte
+++ src/components/Code.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
   export let lang: string;
+  export let value: string;
   export let inline: boolean = false;
 </script>
 
 {#if !inline}
 <pre>
-  <code class="lang-{lang}"><slot /></code>
+  <code class="lang-{lang}">{value}</code>
 </pre>
 {:else}
-  <code class="lang-{lang}"><slot /></code>
+  <code class="lang-{lang}">{value}</code>
 {/if}
